5e15b0d839ff2f61df5207c794b6330a5f566fe5
gasbriel07sm <gasmiranda07@gmail.com>
2025-10-23 10:09:25 -0300

    Exercício resolvido 1 - Estrutura Sequencial

---
+++ src/estrutura_sequencial/exercicio_resolvido1.java
@@ -0,0 +1,33 @@
+package estrutura_sequencial;
+
+/* Enunciado:
+ * Fazer um programa para ler as medidas da largura e comprimento de um terreno
+ * retangular com uma casa decimal, bem como o valor do metro quadrado do terreno
+ * com duas casas decimais. Em seguida, o programa deve mostrar o valor da área do
+ * terreno, bem como o valor do preço do terreno, ambos com duas casas decimais
+*/
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicio_resolvido1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // leitura de dados
+        double largura = sc.nextDouble();
+        double comprimento = sc.nextDouble();
+        double metroQuadrado = sc.nextDouble();
+
+        // processamento de dados
+        double area = largura * comprimento;
+        double preco = area * metroQuadrado;
+
+        // saída
+        System.out.printf("AREA = %.2f%n", area);
+        System.out.printf("PRECO = %.2f%n", preco);
+
+        sc.close();
+    }
+}
